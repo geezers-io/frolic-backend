47584a9dbbc13c80dd3716b9d412cbb9fad0814a
최은기 <galaxyhi4276@gmail.com>
2022-11-12 21:04:27 +0900

    feat: 댓글 레포지토리 지원 메서드 추가 작성

--- src/main/java/com/modular/restfulserver/article/repository/CommentRepository.java
+++ src/main/java/com/modular/restfulserver/article/repository/CommentRepository.java
@@ -1,9 +1,14 @@
 package com.modular.restfulserver.article.repository;
 
+import com.modular.restfulserver.article.model.Article;
 import com.modular.restfulserver.article.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+  List<Comment> findAllByArticle(Article article);
+
 }
