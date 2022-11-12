60af39ec35ae0a727dbf02e381fa6dc9287c66c4
최은기 <galaxyhi4276@gmail.com>
2022-11-12 18:08:39 +0900

    fet: 게시글 작성 요청 dto 클래스 작성

---
+++ src/main/java/com/modular/restfulserver/article/dto/CreatePostRequestDto.java
@@ -0,0 +1,41 @@
+package com.modular.restfulserver.article.dto;
+
+import com.modular.restfulserver.article.model.Article;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.util.Assert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+public class CreatePostRequestDto {
+
+  @NotEmpty
+  @Max(value = 50, message = "게시글 제목은 50글자 미만이어야 합니다.")
+  private final String title;
+
+  @NotEmpty
+  @Max(value = 1000, message = "게시글 본문은 1000글자 미만이어야 합니다.")
+  private final String textContent;
+
+  @Builder(setterPrefix = "add")
+  public CreatePostRequestDto(
+    String title,
+    String textContent
+  ) {
+    Assert.hasText(title, "title field must be not empty");
+    Assert.hasText(textContent, "textContent field must be not empty");
+
+    this.title = title;
+    this.textContent = textContent;
+  }
+
+  public Article toEntity() {
+    return Article.builder()
+      .addTitle(title)
+      .addTextContent(textContent)
+      .build();
+  }
+
+}
