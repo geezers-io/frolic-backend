c3906fcd5df481bd10c7c1ce9c8d5228e0450b62
최은기 <galaxyhi4276@gmail.com>
2022-11-13 23:01:52 +0900

    feat: 해시태그 관련 레포지토리 기능 추가

---
+++ src/main/java/com/modular/restfulserver/article/repository/ArticleHashtagRepository.java
@@ -0,0 +1,15 @@
+package com.modular.restfulserver.article.repository;
+
+import com.modular.restfulserver.article.model.Article;
+import com.modular.restfulserver.article.model.ArticleHashTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ArticleHashtagRepository extends JpaRepository<ArticleHashTag, Long> {
+
+  @Query("select h.name from hashtags h join article_hashtags ah on ah.article = ?1")
+  List<String> findAllByArticle(Article article);
+
+}
--- src/main/java/com/modular/restfulserver/article/repository/HashtagRepository.java
+++ src/main/java/com/modular/restfulserver/article/repository/HashtagRepository.java
@@ -1,17 +1,17 @@
 package com.modular.restfulserver.article.repository;
 
-import com.modular.restfulserver.article.model.Article;
 import com.modular.restfulserver.article.model.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 
-  @Query("select h.name from hashtags h where h.article = ?1")
-  List<String> findAllByArticle(Article article);
+  boolean existsByName(String name);
+
+  Optional<Hashtag> findByName(String tagName);
 
 }
