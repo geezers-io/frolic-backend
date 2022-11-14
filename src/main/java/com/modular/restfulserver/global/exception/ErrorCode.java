eff5037138037e5db785844798d3d55ec9151963
최은기 <galaxyhi4276@gmail.com>
2022-11-14 23:46:24 +0900

    feat: 이미 좋아요한 자원에 좋아요 요청 시 예외 처리 클래스 작성

--- src/main/java/com/modular/restfulserver/global/exception/ErrorCode.java
+++ src/main/java/com/modular/restfulserver/global/exception/ErrorCode.java
@@ -11,6 +11,7 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
   ALREADY_EXISTS_USER(BAD_REQUEST, "이미 존재하는 회원 정보입니다."),
   ALREADY_FOLLOW_INFO(BAD_REQUEST, "이미 팔로우된 사용자입니다."),
+  ALREADY_LIKE_INFO(BAD_REQUEST, "이미 좋아요 한 게시글입니다."),
   PASSWORD_NOT_MATCH(BAD_REQUEST, "패스워드 정보가 일치하지 않습니다."),
   USER_NOT_FOUND(BAD_REQUEST, "존재하지 않는 유저입니다."),
   NONE_TOKEN_ON_HEADER(BAD_REQUEST, "토큰 값이 헤더에 존재하지 않습니다"),
