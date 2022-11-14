5b33de68765bc0b0f798b110b3470b4799d81c23
최은기 <galaxyhi4276@gmail.com>
2022-11-14 23:46:36 +0900

    feat: 좋아요 레포지토리 지원 메서드 추가

--- src/main/java/com/modular/restfulserver/article/repository/LikeRepository.java
+++ src/main/java/com/modular/restfulserver/article/repository/LikeRepository.java
@@ -6,11 +6,17 @@ import com.modular.restfulserver.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
   Long countAllByUser(User user);
 
   Long countAllByArticle(Article article);
+
+  Optional<Like> findByArticleAndUser(Article article, User user);
+
+  boolean existsByArticleAndUser(Article article, User user);
 
 }
