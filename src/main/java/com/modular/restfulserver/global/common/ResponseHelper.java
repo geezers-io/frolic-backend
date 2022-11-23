8921bc5e863825a7fde7d73de15668b8c5198807
최은기 <galaxyhi4276@gmail.com>
2022-11-24 08:52:52 +0900

    feat: 데이터 응답 유틸리티 스태틱 클래스 작성

---
+++ src/main/java/com/modular/restfulserver/global/common/ResponseHelper.java
@@ -0,0 +1,18 @@
+package com.modular.restfulserver.global.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHelper {
+
+  /**
+   * 클라이언트에 전달 할 객체를 { data: ... } 로 감 싼 테이블로 반환합니다.
+   * @param data 클라이언트에 전달할 객체
+   */
+  public static <T> Map<String, T> createDataMap(T data) {
+    Map<String, T> map = new HashMap<>();
+    map.put("data", data);
+    return map;
+  }
+
+}
