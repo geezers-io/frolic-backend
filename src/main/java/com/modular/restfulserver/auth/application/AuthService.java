4befd80ee721f9370d0de0f08ee1a6699bd812c9
최은기 <galaxyhi4276@gmail.com>
2022-11-24 08:55:03 +0900

    feat: 회원가입 및 로그인 시 사용자 객체 반환

--- src/main/java/com/modular/restfulserver/auth/application/AuthService.java
+++ src/main/java/com/modular/restfulserver/auth/application/AuthService.java
@@ -8,13 +8,13 @@ import com.modular.restfulserver.auth.exception.InvalidTokenException;
 import com.modular.restfulserver.auth.exception.PasswordNotMatchException;
 import com.modular.restfulserver.global.config.security.CustomEmailPasswordAuthToken;
 import com.modular.restfulserver.global.config.security.JwtProvider;
+import com.modular.restfulserver.user.dto.UserInfoForClientDto;
+import com.modular.restfulserver.user.exception.UserNotFoundException;
 import com.modular.restfulserver.user.model.User;
 import com.modular.restfulserver.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 
-@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -33,44 +32,45 @@ public class AuthService {
   private final PasswordEncoder passwordEncoder;
   private final AuthenticationManager authenticationManager;
 
-  public void saveUser(UserSignupRequestDto dto) {
+  public TokenResponseDto saveUser(UserSignupRequestDto dto) {
     boolean isExistsEmail = userRepository.existsByEmail(dto.getEmail());
     boolean isExistsUsername = userRepository.existsByUsername(dto.getUsername());
     if (isExistsEmail || isExistsUsername)
       throw new AlreadyExistsUserException();
 
-    dto.setEncodePassword(passwordEncoder.encode(dto.getPassword()));
+    String rawPassword = dto.getPassword();
+    String encodedPassword = passwordEncoder.encode(rawPassword);
+    dto.setEncodePassword(encodedPassword);
     userRepository.save(dto.toEntity());
+
+    UserLoginRequestDto userLoginRequestDto = UserLoginRequestDto.builder()
+      .addUsername(dto.getUsername())
+      .addEmail(dto.getEmail())
+      .addPassword(rawPassword)
+      .build();
+    return this.loginUser(userLoginRequestDto);
   }
 
-  public Map<String, TokenResponseDto> loginUser(UserLoginRequestDto dto) {
-    Map<String, TokenResponseDto> response = new HashMap<>();
-    User user = userRepository.findByEmail(dto.getEmail())
-      .orElseThrow(
-        () -> new UsernameNotFoundException("존재하지 않는 이메일 사용자입니다.")
-      );
+  public TokenResponseDto loginUser(UserLoginRequestDto dto) {
+    User user = userRepository.findByEmail(dto.getEmail()).orElseThrow(UserNotFoundException::new);
 
     if (!passwordEncoder.matches(dto.getPassword(), user.getPassword()))
       throw new PasswordNotMatchException();
 
-    CustomEmailPasswordAuthToken emailPasswordAuthToken =
-      new CustomEmailPasswordAuthToken(dto.getEmail());
-    Authentication authentication = authenticationManager
-      .authenticate(emailPasswordAuthToken);
+    CustomEmailPasswordAuthToken emailPasswordAuthToken = new CustomEmailPasswordAuthToken(dto.getEmail());
+    Authentication authentication = authenticationManager.authenticate(emailPasswordAuthToken);
 
     String email = authentication.getName();
 
     String accessToken = jwtProvider.createAccessToken(email);
     String refreshToken = jwtProvider.createRefreshToken(email);
-
     user.updateRefreshToken(refreshToken);
-    TokenResponseDto tokenDto = TokenResponseDto.builder()
+
+    return TokenResponseDto.builder()
       .addAccessToken(accessToken)
       .addRefreshToken(refreshToken)
+      .addUserInfo(UserInfoForClientDto.from(user))
       .build();
-    response.put("data", tokenDto);
-
-    return response;
   }
 
   public Map<String, String> refresh(String refreshToken) {
@@ -87,7 +87,7 @@ public class AuthService {
   public void logout(String token) {
     String email = jwtProvider.getUserEmailByToken(token);
     User user = userRepository.findByEmail(email)
-      .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저이름입니다."));
+      .orElseThrow(UserNotFoundException::new);
     user.updateRefreshToken(null);
   }
 
