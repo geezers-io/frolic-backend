f3cc3e80d139b371641f7e443057e7d6992ea001
최은기 <galaxyhi4276@gmail.com>
2022-11-25 15:44:15 +0900

    feat: 파일 이름을 자동으로 난수 값과 함께 받아주는 파일 커스텀 클래스 작성

---
+++ src/main/java/com/modular/restfulserver/global/common/file/application/CustomFile.java
@@ -0,0 +1,24 @@
+package com.modular.restfulserver.global.common.file.application;
+
+import com.modular.restfulserver.global.common.file.exception.FaultFileExtensionException;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+public class CustomFile {
+  private final MultipartFile file;
+  private final String customFilename;
+
+  public CustomFile(MultipartFile file) {
+    this.file = file;
+    String filename = file.getOriginalFilename();
+    // TODO: 2022-11-25 확장자가 잘못된 형식에 대해 예외 처리 필요 
+    int splitIndex = filename.indexOf(".");
+    String ext = filename.substring(splitIndex);
+    this.customFilename = filename.substring(0, splitIndex) + "_" + UUID.randomUUID() + ext;
+  }
+
+}
