cac3393d620b2628abe03e8d45ed1df2eee5e579
최은기 <galaxyhi4276@gmail.com>
2022-11-25 21:24:48 +0900

    feat: 게시글 레포지토리에 조인 JPQL 지원하는 메서드 작성

--- src/main/java/com/modular/restfulserver/article/repository/ArticleRepository.java
+++ src/main/java/com/modular/restfulserver/article/repository/ArticleRepository.java
@@ -5,14 +5,21 @@ import com.modular.restfulserver.user.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
 
   Long countAllByUser(User user);
 
   Page<Article> findAllByUserOrderByCreatedDate(User user, Pageable pageable);
+
+  @Query("select at from articles at join article_hashtags ah on at = ah.article where ah.hashtag = (select h from hashtags h where h.name in ?1) order by at.createdDate desc")
+  Page<Article> findAllByHashtagByCreatedDate(List<String> searchList, Pageable pageable);
 
 }
