e0dda3c874b93960f2d741f092a2a9a93844e269
최은기 <galaxyhi4276@gmail.com>
2023-01-01 20:14:37 +0900

    feat: remove unsued unit api code

--- src/test/java/com/modular/restfulserver/article/model/ArticleTest.java
+++ src/test/java/com/modular/restfulserver/article/model/ArticleTest.java
@@ -3,8 +3,6 @@ package com.modular.restfulserver.article.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ArticleTest {
 
   @Test
