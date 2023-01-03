55fb8eea26acda57e8bb6cbadf9a09698f59e01e
최은기 <galaxyhi4276@gmail.com>
2023-01-03 23:15:21 +0900

    feat: apply constant value to docs api code

--- src/main/java/com/modular/restfulserver/auth/swagger/LogoutDocs.java
+++ src/main/java/com/modular/restfulserver/auth/swagger/LogoutDocs.java
@@ -1,6 +1,7 @@
 package com.modular.restfulserver.auth.swagger;
 
 import com.modular.restfulserver.global.swagger.CommonAuthError;
+import com.modular.restfulserver.global.swagger.SwaggerMessageUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
@@ -9,7 +10,7 @@ import java.lang.annotation.*;
 @Operation(
   summary = "로그아웃 요청",
   description = "인증 사용자의 검증 토큰을 서버 보관소에서 삭제합니다.",
-  tags = "인증 및 인가 API",
+  tags = SwaggerMessageUtils.AuthApi,
   security = { @SecurityRequirement(name = "bearer-key") }
 )
 @CommonAuthError
--- src/main/java/com/modular/restfulserver/auth/swagger/ReissueTokenDocs.java
+++ src/main/java/com/modular/restfulserver/auth/swagger/ReissueTokenDocs.java
@@ -1,6 +1,7 @@
 package com.modular.restfulserver.auth.swagger;
 
 import com.modular.restfulserver.global.swagger.CommonAuthError;
+import com.modular.restfulserver.global.swagger.SwaggerMessageUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,7 +13,7 @@ import java.lang.annotation.*;
 @Operation(
   summary = "액세스 토큰 재발행",
   description = "검증 토큰을 확인하고 유효하다면 액세스 토큰을 재발행합니다.",
-  tags = "인증 및 인가 API",
+  tags = SwaggerMessageUtils.AuthApi,
   responses = {
     @ApiResponse(
       responseCode = "200",
