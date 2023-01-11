d7e93909a05580bf18013fb81a12c9fd04f6c511
최은기 <galaxyhi4276@gmail.com>
2023-01-11 18:57:57 +0900

    feat: add test that testuser data

---
+++ src/test/java/unit/util/TestUserTest.java
@@ -0,0 +1,28 @@
+package unit.util;
+
+import com.modular.restfulserver.user.model.User;
+import com.modular.restfulserver.util.TestUser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TestUserTest {
+
+  @Test
+  @DisplayName("테스트용 패스워드를 정상적으로 불러온다.")
+  void checkPassword() {
+    assertThat(TestUser.testPassword).isInstanceOf(String.class);
+  }
+
+  @Test
+  @DisplayName("유저 정보 객체를 정상적으로 불러온다.")
+  void checkTestUser() {
+    User user = TestUser.EUNGI.getUser();
+    assertThat(user.getEmail()).isInstanceOf(String.class);
+    assertThat(user.getPassword()).isInstanceOf(String.class);
+    assertThat(user.getUsername()).isInstanceOf(String.class);
+    assertThat(user.getRealname()).isInstanceOf(String.class);
+  }
+
+}
