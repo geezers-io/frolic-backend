8789d0ad8b7cfc42659d2df267a1bfda9ce62b37
최은기 <galaxyhi4276@gmail.com>
2023-01-12 12:47:09 +0900

    fix: unknown parameter in userRepository

--- src/main/java/com/modular/restfulserver/user/repository/UserRepository.java
+++ src/main/java/com/modular/restfulserver/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.modular.restfulserver.user.repository;
 import com.modular.restfulserver.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -17,5 +18,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
   boolean existsByUsername(String username);
 
   @Query("select u.refreshToken from users u where u.email = :email")
-  String getUserRefreshToken(String email);
+  String getUserRefreshToken(@Param("email") String email);
 }
