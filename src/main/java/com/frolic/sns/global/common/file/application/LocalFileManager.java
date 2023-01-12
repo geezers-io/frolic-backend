10a57aa3150872cb21ce84f37c03c31670ac0028
최은기 <galaxyhi4276@gmail.com>
2023-01-12 21:27:02 +0900

    feat: add file upload logical to LocalFileManager class.

--- src/main/java/com/frolic/sns/global/common/file/application/LocalFileManager.java
+++ src/main/java/com/frolic/sns/global/common/file/application/LocalFileManager.java
@@ -1,5 +1,8 @@
-package com.modular.restfulserver.file.application;
+package com.frolic.sns.global.common.file.application;
 
+import com.frolic.sns.global.common.file.exception.FileDownloadFailureException;
+import com.frolic.sns.global.common.file.exception.FileSaveFailException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
@@ -7,46 +10,64 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.Objects;
 
+@Slf4j
 @Service
 public class LocalFileManager implements FileManageable {
 
-  private final String serverUrl;
   private final String uploadDir;
 
   public LocalFileManager(
-    @Value("${server.port}") final String port,
-    @Value("${server.address}") final String address,
-    @Value("${server.protocol}") final String protocol,
     @Value("${custom.path.upload-images}") final String uploadDir
   ) throws IOException {
-    this.serverUrl = protocol + "://" + address + ":" + port;
     this.uploadDir = uploadDir;
-    createUploadDirectoryIfNotExists();
+    createUploadDirectory();
   }
 
   @Override
   public void singleUpload(MultipartFile file) {
-
+    store(file);
   }
 
   @Override
   public void multipleUpload(List<MultipartFile> files) {
-
+    files.forEach(this::store);
   }
 
   @Override
   public UrlResource download(String filename) {
-    return null;
+    String path = uploadDir + "/" + filename;
+    try {
+      return new UrlResource(path);
+    } catch (MalformedURLException exception) {
+      throw new FileDownloadFailureException();
+    }
   }
 
-  private void createUploadDirectoryIfNotExists() throws IOException {
+  private void createUploadDirectory() throws IOException {
     File existsFile = new File(this.uploadDir);
     if (!existsFile.isDirectory() || !existsFile.exists())
       Files.createDirectory(Paths.get(this.uploadDir));
+  }
+
+  private void store(MultipartFile file) {
+    String filename = Objects.requireNonNull(file.getOriginalFilename());
+    String temperedFilename = getTemperedFilename(filename);
+    try (InputStream inputStream = file.getInputStream()) {
+      Path updateDirPath = Paths.get(uploadDir + "/" + temperedFilename);
+      Files.copy(inputStream, updateDirPath, StandardCopyOption.REPLACE_EXISTING);
+    } catch (Exception ex) {
+      log.error(ex.toString());
+      throw new FileSaveFailException();
+    }
   }
 
 }
