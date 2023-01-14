933ad017971b6ed4b2212128d3f7e2b74186d429
최은기 <galaxyhi4276@gmail.com>
2023-01-14 12:27:39 +0900

    feat: add test that store auth code info to cacheDB

---
+++ src/test/java/com/frolic/sns/auth/application/AuthCodeCacheManagerTest.java
@@ -0,0 +1,32 @@
+package com.frolic.sns.auth.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class AuthCodeCacheManagerTest {
+
+  protected final ValueOperations<String, AuthCode.MetaData> cacheKeyValueStore;
+
+  protected final AuthCode authCode = new AuthCode("555555", FinderType.EMAIL);
+  @Autowired
+  AuthCodeCacheManagerTest(ValueOperations<String, AuthCode.MetaData> cacheKeyValueStore) {
+    this.cacheKeyValueStore = cacheKeyValueStore;
+  }
+
+  @Test
+  @DisplayName("키 밸류 스토어에 기존에 삽입했던 인증 코드 객체를 조회할 수 있다.")
+  void getAuthCodeMetadata() {
+    cacheKeyValueStore.set(authCode.getCode(), authCode.getAuthCodeMetaData());
+    AuthCode.MetaData metaData = cacheKeyValueStore.get(authCode.getCode());
+    assert metaData != null;
+    assertThat(metaData.getCountOfAttempts()).isEqualTo(0);
+    assertThat(metaData.getFinderType()).isEqualTo(FinderType.EMAIL);
+  }
+
+}
