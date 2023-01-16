a93e67641ce84a007ad3e4f8f716b2227612acca
최은기 <galaxyhi4276@gmail.com>
2023-01-17 07:53:03 +0900

    feat: change auth policy

--- src/main/java/com/frolic/sns/auth/application/finder/AuthCode.java
+++ src/main/java/com/frolic/sns/auth/application/finder/AuthCode.java
@@ -1,32 +1,54 @@
 package com.frolic.sns.auth.application.finder;
 
+import com.frolic.sns.global.util.message.CommonMessageUtils;
+import io.jsonwebtoken.lang.Assert;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 /**
  * @implNote 인증 코드에 대한 객체입니다.
  */
-@RequiredArgsConstructor
 @Getter
 public class AuthCode {
+  private final UUID id;
   private final String code;
   private final FinderType finderType;
   private final String destination; // 인증되었을 때 사용자 정보에 대해 전달할 정보입니다.
 
   private final int countOfAttempts = 0;
 
+  @Builder(setterPrefix = "add")
+  public AuthCode(UUID id, String code, FinderType finderType, String destination) {
+    Assert.notNull(id, CommonMessageUtils.getIllegalFieldError("id"));
+    Assert.notNull(code, CommonMessageUtils.getIllegalFieldError("code"));
+    Assert.notNull(finderType, CommonMessageUtils.getIllegalFieldError("finderType"));
+    Assert.notNull(destination, CommonMessageUtils.getIllegalFieldError("destination"));
+    this.id = id;
+    this.code = code;
+    this.finderType = finderType;
+    this.destination = destination;
+  }
+
   @NoArgsConstructor
   @Getter
   public static class MetaData implements Serializable {
+    private String code;
+
     private FinderType finderType;
     private int countOfAttempts;
 
     private String destination;
 
-    public MetaData(FinderType type, int countOfAttempts, String destination) {
+    @Builder(setterPrefix = "add")
+    public MetaData(String code, FinderType type, int countOfAttempts, String destination) {
+      Assert.hasText(code, CommonMessageUtils.getIllegalFieldError("code"));
+      Assert.hasText(destination, CommonMessageUtils.getIllegalFieldError("destination"));
+
+      this.code = code;
       this.finderType = type;
       this.countOfAttempts = countOfAttempts;
       this.destination = destination;
@@ -39,7 +61,7 @@ public class AuthCode {
   }
 
   public MetaData getAuthCodeMetaData() {
-    return new MetaData(finderType, countOfAttempts, destination);
+    return new MetaData(code, finderType, countOfAttempts, destination);
   }
 
 }
--- src/main/java/com/frolic/sns/auth/application/finder/UserInfoFindManager.java
+++ src/main/java/com/frolic/sns/auth/application/finder/UserInfoFindManager.java
@@ -4,6 +4,8 @@ import com.frolic.sns.auth.exception.MisMatchFinderTypeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Service
 public class UserInfoFindManager implements UserInfoFindable {
@@ -11,22 +13,49 @@ public class UserInfoFindManager implements UserInfoFindable {
   private final AuthCodeCacheManager authCodeCacheManager;
   private final int TTL_TIME = 10;
 
+  private final int MAX_TRY_COUNT = 5;
+
   @Override
-  public AuthCode.MetaData getAuthCodeMedaData(String code, FinderType finderType) {
-    AuthCode.MetaData metaData = authCodeCacheManager.getAuthenticationCode(code);
+  public AuthCode.MetaData getAuthCode(UUID id, FinderType finderType) {
+    AuthCode.MetaData metaData = authCodeCacheManager.getAuthenticationCode(id);
     if (!metaData.getFinderType().equals(finderType))
       throw new MisMatchFinderTypeException();
     return metaData;
   }
 
   @Override
-  public void storeAuthCode(String code, FinderType finderType, String principalInfo) {
-    AuthCode authCode = new AuthCode(code, finderType, principalInfo);
+  public void storeAuthCode(UUID id, String code, FinderType finderType, String dest) {
+    AuthCode authCode = AuthCode.builder()
+      .addId(id)
+      .addCode(code)
+      .addFinderType(finderType)
+      .addDestination(dest)
+      .build();
     authCodeCacheManager.storeAuthenticationCode(authCode, TTL_TIME);
+  }
+
+  @Override
+  public UUID createId() {
+    return UUID.randomUUID();
+  }
+
+  @Override
+  public void removeAuthCode(UUID id) {
+    authCodeCacheManager.removeAuthenticationCode(id);
+  }
+
+  @Deprecated
+  @Override
+  public String createCode() {
+    return String.valueOf((Math.random() * 100000) + 999999);
   }
 
   public int getTimeToLive() {
     return TTL_TIME;
+  }
+
+  public int getMaxTryCount() {
+    return MAX_TRY_COUNT;
   }
 
 }
