32037e4b255d9d2bcd9cb783e19386e92156283f
최은기 <galaxyhi4276@gmail.com>
2023-01-16 23:36:24 +0900

    fix: fix a potential hazard

--- src/main/java/com/frolic/sns/auth/dto/UserFindEmailRequest.java
+++ src/main/java/com/frolic/sns/auth/dto/UserFindEmailRequest.java
@@ -1,22 +1,14 @@
 package com.frolic.sns.auth.dto;
 
-import com.frolic.sns.user.model.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UserFindEmailRequest {
-    private String phoneNumber;
-    
-    public UserFindEmailRequest(String phoneNumber){
+  private String phoneNumber;
+  public UserFindEmailRequest(String phoneNumber){
         this.phoneNumber = phoneNumber;
-    }
-
-    public User toEntity() {
-        return User.builder()
-                .addPhoneNumber(phoneNumber)
-                .build();
     }
 
 }
