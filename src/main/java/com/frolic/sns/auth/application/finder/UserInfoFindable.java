2dc4c0fb73b9e6039251ec051cffe8676f763784
최은기 <galaxyhi4276@gmail.com>
2023-01-17 19:49:54 +0900

    feat: update builder usage point for using efficient builder pattern

--- src/main/java/com/frolic/sns/auth/application/finder/UserInfoFindable.java
+++ src/main/java/com/frolic/sns/auth/application/finder/UserInfoFindable.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 public interface UserInfoFindable {
   AuthCode.MetaData getAuthCode(UUID id, FinderType finderType);
 
-  void storeAuthCode(UUID id, String code, FinderType finderType, String principalInfo);
+  void storeAuthCode(AuthCode authCode);
 
   UUID createId();
 
