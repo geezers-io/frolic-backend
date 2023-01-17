c0114092ce653e4ffa45d5a503b6450795f3795b
최은기 <galaxyhi4276@gmail.com>
2023-01-17 19:49:16 +0900

    refactor: using .toString()

--- src/main/java/com/frolic/sns/auth/application/finder/AuthCodeCacheManager.java
+++ src/main/java/com/frolic/sns/auth/application/finder/AuthCodeCacheManager.java
@@ -3,13 +3,11 @@ package com.frolic.sns.auth.application.finder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class AuthCodeCacheManager {
 
@@ -20,13 +18,11 @@ public class AuthCodeCacheManager {
    * @param authCode 인증 코드 객체
    */
   public void storeAuthenticationCode(AuthCode authCode, int minutes) {
-    String stringId = getStringId(authCode.getId());
-    redisKeyValueStore.set(stringId, authCode.getAuthCodeMetaData(), minutes, TimeUnit.MINUTES);
+    redisKeyValueStore.set(authCode.getId().toString(), authCode.getAuthCodeMetaData(), minutes, TimeUnit.MINUTES);
   }
 
   public void removeAuthenticationCode(UUID id) {
-    String stringId = getStringId(id);
-    redisKeyValueStore.getAndDelete(stringId);
+    redisKeyValueStore.getAndDelete(id.toString());
   }
 
   /**
@@ -34,11 +30,7 @@ public class AuthCodeCacheManager {
    * @param id 인증코드 정보를 가져올 인증코드 키 입니다.
    */
   public AuthCode.MetaData getAuthenticationCode(UUID id) {
-    return redisKeyValueStore.get(getStringId(id));
-  }
-
-  private String getStringId(UUID id) {
-    return id.toString();
+    return redisKeyValueStore.get(id.toString());
   }
 
 }
