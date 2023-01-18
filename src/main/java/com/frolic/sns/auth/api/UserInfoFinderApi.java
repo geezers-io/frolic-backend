bbb9e7cf2c30ed38ac2ef570bc0b2b6870da3f52
최은기 <galaxyhi4276@gmail.com>
2023-01-18 09:27:41 +0900

    feat: 사용자 이메일 정보 찾기 api 작성

--- src/main/java/com/frolic/sns/auth/api/UserInfoFinderApi.java
+++ src/main/java/com/frolic/sns/auth/api/UserInfoFinderApi.java
@@ -2,6 +2,8 @@ package com.frolic.sns.auth.api;
 
 import com.frolic.sns.auth.application.finder.EmailFindManager;
 import com.frolic.sns.auth.dto.UserFindEmailRequest;
+import com.frolic.sns.auth.dto.VerifyCodeRequest;
+import com.frolic.sns.global.exception.NotFoundCookieException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.UUID;
@@ -27,13 +30,32 @@ public class UserInfoFinderApi {
     HttpServletResponse response
   ) {
     UUID id = emailFindManager.sendAuthCode(request);
-    setEmailSidCookie(response, id);
+    setEmailSidCookie(response, SidType.EMAIL_SID, id);
     return ResponseEntity.status(HttpStatus.OK).build();
   }
 
-  private void setEmailSidCookie(HttpServletResponse response, UUID emailSid) {
-    Cookie cookie = new Cookie("EMAIL_SID", emailSid.toString());
+  @PostMapping("/email/check")
+  public ResponseEntity<Void> verifyEmailAuthCodeApi(
+    @RequestBody @Valid VerifyCodeRequest verifyCodeRequest,
+    HttpServletRequest httpRequest
+  ) {
+    Cookie[] cookies = httpRequest.getCookies();
+    UUID id = parseSidFromCookies(cookies, SidType.EMAIL_SID);
+    emailFindManager.authCodeVerify(id, verifyCodeRequest);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  private void setEmailSidCookie(HttpServletResponse response, SidType sidType, UUID emailSid) {
+    Cookie cookie = new Cookie(sidType.name(), emailSid.toString());
     response.addCookie(cookie);
+  }
+
+  private UUID parseSidFromCookies(Cookie[] cookies, SidType sidType) {
+    for (Cookie cookie : cookies) {
+      if (cookie.getName().equals(sidType.name()))
+        return UUID.fromString(cookie.getValue());
+    }
+    throw new NotFoundCookieException();
   }
 
 }
