c3e22d47ce3780d0e3691825c8587a584d1108e8
rungky <han05081486@gmail.com>
2023-01-22 17:15:52 +0900

    chore: 임시 비밀번호 발급 기능 Query문 작성

--- src/main/java/com/frolic/sns/user/repository/UserRepository.java
+++ src/main/java/com/frolic/sns/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.frolic.sns.user.repository;
 
 import com.frolic.sns.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,4 +26,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
   @Query("select u.email from users u where u.phoneNumber = :phoneNumber")
   Optional<String> getEmailByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 
+  @Query("select u.phoneNumber from users u where u.email = ?1")
+  Optional<String> getPhoneNumberByEmail(@Param("email") String email);
+
+  @Query("select u.email from users u where u.email = ?1 and u.phoneNumber = ?2")
+  Optional<String> getUserInfoPwExist(@Param("email") String email, @Param("phoneNumber") String phoneNumber);
+
+  @Modifying
+  @Transactional
+  @Query("update users u set u.password = ?1 where u.email = ?2 and u.phoneNumber = ?3")
+  void setTempPassword(@Param("password") String password,
+                          @Param("email") String email,
+                          @Param("phoneNumber") String phoneNumber);
 }
