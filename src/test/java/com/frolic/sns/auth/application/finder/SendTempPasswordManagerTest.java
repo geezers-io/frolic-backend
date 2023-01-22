022e8d7916472531c148dcb4ba9a5461ea462b2a
rungky <han05081486@gmail.com>
2023-01-22 17:16:57 +0900

    chore: 임시 비밀번호 발급 기능 테스트 코드 작성중

---
+++ src/test/java/com/frolic/sns/auth/application/finder/SendTempPasswordManagerTest.java
@@ -0,0 +1,84 @@
+package com.frolic.sns.auth.application.finder;
+
+import com.frolic.sns.auth.application.finder.common.AuthCode;
+import com.frolic.sns.auth.application.finder.common.FinderType;
+import com.frolic.sns.auth.dto.VerifyCodeRequest;
+import com.frolic.sns.auth.exception.MisMatchAuthCodeException;
+import com.frolic.sns.auth.exception.OverTriedAuthCodeException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class SendTempPasswordManagerTest {
+
+    @Autowired
+    protected SendTempPasswordManager sendTempPasswordManager;
+
+    UUID id;
+
+    @BeforeEach
+    void beforeEach() {
+        id = sendTempPasswordManager.createId();
+        String code = sendTempPasswordManager.createCode();
+        sendTempPasswordManager.storeAuthCode(
+                AuthCode.builder()
+                        .addId(id)
+                        .addCode(code)
+                        .addDestination("han14866@naver.com")
+                        .addDestination("01051998927")
+                        .addCountOfAttempts(0)
+                        .addFinderType(FinderType.PASSWORD)
+                        .addLocalTime(LocalTime.now())
+                        .build()
+        );
+    }
+
+    @Test
+    @DisplayName("사용자가 저장한 인증 코드 값을 정상적으로 불러온다.")
+    void createAndLoadAuthCode() {
+        // given
+
+        // when
+        AuthCode.MetaData metaData = sendTempPasswordManager.getAuthCode(id, FinderType.PASSWORD);
+
+        // then
+        Assertions.assertNotNull(metaData);
+        System.out.println("metaData.destination: " + metaData.getDestination());
+    }
+
+    @Test
+    @DisplayName("인증 코드 검증을 실패한다.")
+    void verifyOnceFailed() {
+        // given
+        VerifyCodeRequest request = new VerifyCodeRequest("wrongCode");
+
+        // then
+        assertThrows(MisMatchAuthCodeException.class, () -> sendTempPasswordManager.authCodeVerify(id, request));
+    }
+
+    @Test
+    @DisplayName("인증 코드 시도가 5회 초과 시 에러가 발생한다.")
+    void verifyOver() {
+        // given
+        VerifyCodeRequest request = new VerifyCodeRequest("wrongCode");
+
+        // when
+        for (int i = 0; i < 5; i++) {
+            assertThrows(MisMatchAuthCodeException.class, () -> {
+                sendTempPasswordManager.authCodeVerify(id, request);
+            });
+        }
+
+        // then
+        assertThrows(OverTriedAuthCodeException.class, () -> sendTempPasswordManager.authCodeVerify(id, request));
+    }
+}
