b6e46a55a80caeb58fddcc6522987d9e6dda7fb0
최은기 <galaxyhi4276@gmail.com>
2023-02-02 16:57:37 +0900

    feat: 기존 빈 초기화 객체 재조정

---
+++ src/main/java/com/frolic/sns/global/config/spring/JavaMailerConfiguration.java
@@ -0,0 +1,34 @@
+package com.frolic.sns.global.config.spring;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JavaMailerConfiguration {
+
+  @Value("${spring.mail.host}")
+  private String host;
+
+  @Value("${spring.mail.username}")
+  private String username;
+
+  @Value("${spring.mail.port}")
+  private Integer port;
+
+  @Value("${spring.mail.password}")
+  private String password;
+
+  @Bean
+  public JavaMailSender javaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost(host);
+    mailSender.setPort(port);
+    mailSender.setUsername(username);
+    mailSender.setPassword(password);
+    return mailSender;
+  }
+
+}
