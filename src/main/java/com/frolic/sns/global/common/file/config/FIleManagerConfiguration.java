9b7ed95d725e0b575029cf04e3b197c59244b245
최은기 <galaxyhi4276@gmail.com>
2023-02-02 17:00:40 +0900

    feat: 초기화 빈 로직 업데이트

--- src/main/java/com/frolic/sns/global/common/file/config/FIleManagerConfiguration.java
+++ src/main/java/com/frolic/sns/global/common/file/config/FIleManagerConfiguration.java
@@ -4,6 +4,8 @@ import com.frolic.sns.global.common.file.application.FileManageable;
 import com.frolic.sns.global.common.file.application.LocalFileManager;
 import com.frolic.sns.global.common.file.application.S3FileManager;
 import com.frolic.sns.global.common.file.repository.FileRepository;
+import com.frolic.sns.global.config.security.JwtProvider;
+import com.frolic.sns.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +17,10 @@ import org.springframework.context.annotation.Configuration;
 public class FIleManagerConfiguration {
   private final FileRepository fileRepository;
 
+  private final UserRepository userRepository;
+
+  private final JwtProvider jwtProvider;
+
   @Value("${spring.profiles.active}")
   private String profile;
 
@@ -22,7 +28,7 @@ public class FIleManagerConfiguration {
     log.error("profile: {}", profile);
     if (profile.equals("local")) {
       log.info("파일 관리자 객체가 LocalFileManager 로 생성되었습니다.");
-      return new LocalFileManager(fileRepository);
+      return new LocalFileManager(fileRepository, userRepository, jwtProvider);
     } else {
       log.info("파일 관리자 객체가 S3FileManager 로 생성되었습니다.");
       return new S3FileManager(fileRepository);
