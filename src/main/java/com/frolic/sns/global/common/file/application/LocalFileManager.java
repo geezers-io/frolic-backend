3c67bceedd24136254d0747ac09694a978af63dc
최은기 <galaxyhi4276@gmail.com>
2023-02-09 09:38:38 +0900

    feat: 중복 빈 객체에 대한 더 나은 대안 마련

--- src/main/java/com/frolic/sns/global/common/file/application/LocalFileManager.java
+++ src/main/java/com/frolic/sns/global/common/file/application/LocalFileManager.java
@@ -5,14 +5,10 @@ import com.frolic.sns.global.common.file.exception.FileDownloadFailureException;
 import com.frolic.sns.global.common.file.exception.FileSaveFailException;
 import com.frolic.sns.global.common.file.model.ApplicationFile;
 import com.frolic.sns.global.common.file.repository.FileRepository;
-import com.frolic.sns.global.config.security.JwtProvider;
-import com.frolic.sns.user.exception.UserNotFoundException;
-import com.frolic.sns.user.model.User;
-import com.frolic.sns.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,7 +26,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-@Primary
+@Qualifier("LocalFileManager")
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -84,27 +80,27 @@ public class LocalFileManager implements FileManageable {
   private FileInfo store(MultipartFile file) {
     String filename = Objects.requireNonNull(file.getOriginalFilename());
     String temperedFilename = getTemperedFilename(filename);
+    return createFile(file, temperedFilename);
+  }
 
+  private FileInfo createFile(MultipartFile file, String temperedName) {
     try (InputStream inputStream = file.getInputStream()) {
-      Path updateDirPath = Paths.get(uploadDir + "/" + temperedFilename);
+      Path updateDirPath = Paths.get(uploadDir + "/" + temperedName);
       Files.copy(inputStream, updateDirPath, StandardCopyOption.REPLACE_EXISTING);
-      String downloadUrl = protocol + "://" + host + ":" + port + "/images/" + temperedFilename;
-      ApplicationFile applicationFile = ApplicationFile.builder()
-        .addName(temperedFilename)
-        .addSize(file.getSize())
-        .addDownloadUrl(downloadUrl)
-        .build();
-      Long id = fileRepository.saveAndFlush(applicationFile).getId();
-      return FileInfo.builder()
-        .addId(id)
-        .addDownloadUrl(downloadUrl)
-        .addFilename(temperedFilename)
-        .build();
-
+      return FileInfo.from(createFileModel(file, temperedName));
     } catch (Exception ex) {
       log.error(ex.toString());
       throw new FileSaveFailException();
     }
+  }
+
+  private ApplicationFile createFileModel(MultipartFile file, String name) {
+    ApplicationFile applicationFile = ApplicationFile.builder()
+      .addName(name)
+      .addSize(file.getSize())
+      .addDownloadUrl(protocol + "://" + host + ":" + port + "/images/" + name)
+      .build();
+    return fileRepository.saveAndFlush(applicationFile);
   }
 
 }
--- src/main/java/com/frolic/sns/global/common/file/application/S3FileManager.java
+++ src/main/java/com/frolic/sns/global/common/file/application/S3FileManager.java
@@ -3,12 +3,14 @@ package com.frolic.sns.global.common.file.application;
 import com.frolic.sns.global.common.file.dto.FileInfo;
 import com.frolic.sns.global.common.file.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Qualifier("S3FileManager")
 @Service
 @RequiredArgsConstructor
 public class S3FileManager implements FileManageable {
