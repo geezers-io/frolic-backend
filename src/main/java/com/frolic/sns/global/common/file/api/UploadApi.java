5b2b1044253940916b7741ee8cb755b458ea707a
galaxy4276 <galaxyhi4276@gmail.com>
2023-02-09 09:57:46 +0900

    feat: 파일 관리 객체 설계 개선 (#44)
    * feat: 환경에 따른 파일 관리 객체 초기화 기능 제거
    
    * feat: 중복 빈 객체에 대한 더 나은 대안 마련
    
    * feat: 파일 정보 객체를 엔티티 정보로 생성할 수 있는 정적 메서드 작성

--- src/main/java/com/frolic/sns/global/common/file/api/UploadApi.java
+++ src/main/java/com/frolic/sns/global/common/file/api/UploadApi.java
@@ -3,23 +3,24 @@ package com.frolic.sns.global.common.file.api;
 import com.frolic.sns.global.common.ResponseHelper;
 import com.frolic.sns.global.common.file.application.FileManageable;
 import com.frolic.sns.global.common.file.dto.FileInfo;
-import com.frolic.sns.global.config.security.JwtProvider;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RequestMapping("/api/file")
-@RequiredArgsConstructor
 @RestController
 public class UploadApi {
 
   private final FileManageable fileManager;
+
+  public UploadApi(@Qualifier("LocalFileManager") FileManageable fileManager) {
+    this.fileManager = fileManager;
+  }
 
   @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
   public ResponseEntity<Map<String, FileInfo>> uploadFileApi(@RequestPart MultipartFile file) {
