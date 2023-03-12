ce482edff94bfcfcb3b1e4a6968c6699a85b217f
galaxy4276 <galaxyhi4276@gmail.com>
2023-03-12 15:48:29 +0900

    feat: ExtensionValidator 구현체에 변경내용 반영

--- src/main/java/com/frolic/sns/global/common/file/application/ImageFileExtensionValidator.java
+++ src/main/java/com/frolic/sns/global/common/file/application/ImageFileExtensionValidator.java
@@ -5,18 +5,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Component
 public final class ImageFileExtensionValidator implements FileExtensionValidator {
-
-  private final List<String> extensionNameList = Arrays.asList("jpg", "jpeg", "png", "gif", "bmp", "svg", "webp");
 
   public void validate(MultipartFile file) {
     if (file == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "요청 파일 형식이 잘못되었습니다.");
     String extensionName = getExtensionName(file);
-    if (!extensionNameList.contains(extensionName)) {
+    if (!imageExtensionNameList.contains(extensionName)) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미지 파일 확장자가 아닙니다.");
     }
   }
