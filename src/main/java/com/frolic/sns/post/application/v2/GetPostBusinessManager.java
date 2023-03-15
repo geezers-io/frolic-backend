416888cf853047ad0ee62cb1fd78e987f6211cf2
galaxy4276 <galaxyhi4276@gmail.com>
2023-03-15 11:55:36 +0900

    refactor: cursorId 의 상태 기반으로 나눠쓰던 메서드를 통합 ( 재사용 코드 )

--- src/main/java/com/frolic/sns/post/application/v2/GetPostBusinessManager.java
+++ src/main/java/com/frolic/sns/post/application/v2/GetPostBusinessManager.java
@@ -4,7 +4,6 @@ import com.frolic.sns.post.dto.v2.PostInfo;
 import com.frolic.sns.post.model.Post;
 import com.frolic.sns.post.repository.*;
 import com.frolic.sns.user.dto.UserInfo;
-import com.frolic.sns.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -18,15 +17,15 @@ public class GetPostBusinessManager {
   private final LikeRepository likeRepository;
   private final CommentDslRepository commentDslRepository;
 
-  public List<PostInfo> createPostInfos(List<Post> posts, User user) {
+  public List<PostInfo> createPostInfos(List<Post> posts) {
     return posts.stream().map(
       post -> PostInfo.addProperties(post)
         .addCommentCount(
           commentDslRepository.getCommentCount(post.getId())
         )
         .addLikeCount(likeRepository.countAllByPost(post))
-        .addIsLikeUp(likeRepository.existsByPostAndUser(post, user))
-        .addUserInfo(UserInfo.from(user))
+        .addIsLikeUp(likeRepository.existsByPostAndUser(post, post.getUser()))
+        .addUserInfo(UserInfo.from(post.getUser()))
         .build()
     )
       .collect(Collectors.toList());
