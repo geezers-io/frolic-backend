8f7a8937fc4f4f82f48b908ca2c5d9baf6f9009b
galaxy4276 <galaxyhi4276@gmail.com>
2023-03-18 14:59:12 +0900

    feat: 사용하지 않는 로직 제거 및 기존 시큐리티 로직 auth 패키지로 이동

--- src/main/java/com/frolic/sns/global/config/security/JwtConstants.java
+++
@@ -1,8 +0,0 @@
-package com.frolic.sns.global.config.security;
-
-
-public class JwtConstants {
-  public static final String TOKEN_HEADER_PREFIX = "Bearer ";
-  public static final String ACCESS_TOKEN = "accessToken";
-  public static final String REFRESH_TOKEN = "refreshToken";
-}
